fb8c2aa059e45f881dbd15926612bca94c8a9a7c
M4D-Programmer <donaven1022@gamil.com>
2026-07-22 00:06:32 -0400

    Made ItemDetails page Dynamic

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,16 +1,67 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import Skeleton from "../components/UI/Skeleton";
+import axios from "axios";
 
 const ItemDetails = () => {
   const { nftId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [ITEM, setITEM] = useState(null);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    let isMounted = true;
 
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const ItemResponse = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+        );
+
+        if (!isMounted) return;
+
+        const data = ItemResponse.data;
+        const api = Array.isArray(data) && data.length > 0 ? data[0] : data;
+
+        const ItemDetails = {
+          ...api,
+          nftImage: api?.nftImage,
+          nftId: api?.nftId || nftId,
+          nftTag: api?.tag || "art",
+          nftTitle: api?.title || "Rainbow Style",
+          nftDescription: api?.description || "No description available.",
+          nftPrice: api?.price || "1.85",
+          nftOwner: api?.ownerName || "Monica Lucas",
+          nftOwnerImage: api?.ownerImage || AuthorImage,
+          nftOwnerId: api?.ownerId || "1",
+          nftCreator: api?.creatorName || "Monica Lucas",
+          nftCreatorImage: api?.creatorImage || AuthorImage,
+          nftCreatorId: api?.creatorId || "1",
+          nftViews: api?.views ?? 100,
+          nftLikes: api?.likes ?? 74,
+        };
+
+        setITEM(ItemDetails);
+      } catch (error) {
+        console.error("Error fetching item data:", error);
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [nftId]);
+    
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -19,43 +70,70 @@ const ItemDetails = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
+                {loading || !ITEM ? (
+                  <Skeleton width="640px" height="640px" className="me-3" />
+                ) : (
+                  <img
+                    src={ITEM.nftImage}
+                    className="img-fluid img-rounded mb-sm-30 nft-image"
+                    alt={ITEM.nftTitle}
+                  />
+                )}
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style {nftId ? `#${nftId}` : "#194"}</h2>
+                  {loading || !ITEM ? (
+                    <>
+                      <Skeleton width="60%" height="36px" className="mb-3" />
+                      <div className="item_info_counts">
+                        <div className="item_info_views">
+                          <i className="fa fa-eye"></i>
+                          <Skeleton width="40px" height="16px" />
+                        </div>
+                        <div className="item_info_like">
+                          <i className="fa fa-heart"></i>
+                          <Skeleton width="40px" height="16px" />
+                        </div>
+                      </div>
+                      <Skeleton width="100%" height="56px" className="mt-3" />
+                    </>
+                  ) : (
+                    <>
+                      <h2>{ITEM?.nftTitle} #{ITEM?.nftTag}</h2>
 
-                  <div className="item_info_counts">
-                    <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
-                    </div>
-                    <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
-                    </div>
-                  </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+                      <div className="item_info_counts">
+                        <div className="item_info_views">
+                          <i className="fa fa-eye"></i>
+                          {ITEM?.nftViews}
+                        </div>
+                        <div className="item_info_like">
+                          <i className="fa fa-heart"></i>
+                          {ITEM?.nftLikes}
+                        </div>
+                      </div>
+                      <p>{ITEM?.nftDescription}</p>
+                    </>
+                  )}
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {loading || !ITEM ? (
+                            <Skeleton width="60px" height="60px" borderRadius="50%" />
+                          ) : (
+                            <Link to={`/authors/${ITEM?.nftOwnerId}`}>
+                              <img className="lazy" src={ITEM?.nftOwnerImage} alt="" />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          {loading || !ITEM ? (
+                            <Skeleton width="120px" height="18px" />
+                          ) : (
+                            <Link to={`/authors/${ITEM?.nftOwnerId}`}>{ITEM?.nftOwner}</Link>
+                          )}
                         </div>
                       </div>
                     </div>
@@ -66,13 +144,21 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {loading || !ITEM ? (
+                            <Skeleton width="60px" height="60px" borderRadius="50%" />
+                          ) : (
+                            <Link to={`/authors/${ITEM?.nftCreatorId}`}>
+                              <img className="lazy" src={ITEM?.nftCreatorImage} alt="" />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                            {loading || !ITEM ? (
+                              <Skeleton width="120px" height="18px" />
+                            ) : (
+                              <Link to={`/authors/${ITEM?.nftCreatorId}`}>{ITEM?.nftCreator}</Link>
+                            )}
                         </div>
                       </div>
                     </div>
@@ -80,7 +166,11 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      {loading || !ITEM ? (
+                        <Skeleton width="40px" height="18px" />
+                      ) : (
+                        <span>{ITEM?.nftPrice}</span>
+                      )}
                     </div>
                   </div>
                 </div>
